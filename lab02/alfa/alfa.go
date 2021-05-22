a130e0ab39f400367d0809f4e4acda7aabe706f8
hamed <hamediran1999@gmail.com>
2021-05-22 22:06:28 +0430

    go lang varibale

---
+++ lab02/alfa/alfa.go
@@ -0,0 +1,16 @@
+package alfa 
+import "fmt"
+func Alfa(){
+	s := 25
+	d := 13.25
+	na := "hamed"
+	fa := false
+	fmt.Println(s,d,na,fa)
+	var int1,int2,int3 int
+	int1 =1
+	int2 =2
+	int3 =3
+	fmt.Println(int1,int2,int3)
+
+  }
+
---
+++ lab02/main.go
@@ -0,0 +1,18 @@
+package main
+import (
+  "fmt"
+  "var/alfa"
+)
+
+func main() {
+  var s string = "Hello, World"
+  var i int = 8
+  var fa bool
+  var nu float32 = 8.25
+  
+
+    fmt.Println(s,i,fa,nu)
+    fmt.Println("\n")
+    alfa.Alfa()
+
+}
